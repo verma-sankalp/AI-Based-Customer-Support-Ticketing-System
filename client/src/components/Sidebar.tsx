0f4105f7dae1e5b8e22d69b4a6f388077a63d11d
Sankalp <sankalpverma2111@gmail.com>
2026-06-12 20:56:12 +0530

    client/src/components/Sidebar.tsx

---
+++ client/src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <ul>
+        <li>Dashboard</li>
+        <li>Tickets</li>
+        <li>Analytics</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+}
